1c76fb2339d2b01c69dff27c3507a347979fe2a7
deepakgrandhi <deepakgrandhi@gmail.com>
2026-07-21 11:22:05 +0530

    updated key-value pairs in interceptor

--- lib/src/network/auth_interceptor.dart
+++ lib/src/network/auth_interceptor.dart
@@ -8,15 +8,19 @@ import 'package:tradeable_flutter_sdk/tradeable_flutter_sdk.dart';
 class AuthInterceptor extends Interceptor {
   @override
   void onRequest(
-      RequestOptions options, RequestInterceptorHandler handler) async {
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
     final portalToken = TFS().portalToken;
     if (portalToken != null) {
       options.headers['Authorization'] = TFS().authorization ?? '';
       options.headers['x-api-client-id'] = TFS().appId ?? '';
-      options.headers['x-subAccountID'] = TFS().clientId ?? '';
-      options.headers['x-aslToken'] = TFS().portalToken ?? '';
-      options.headers['x-api-encryption-key'] =
-          encryptRsa(TFS().secretKey ?? "", TFS().publicKey ?? "");
+      options.headers['X-SubAccountID'] = TFS().clientId ?? '';
+      options.headers['X-AuthToken'] = TFS().portalToken ?? '';
+      options.headers['x-api-encryption-key'] = encryptRsa(
+        TFS().secretKey ?? "",
+        TFS().publicKey ?? "",
+      );
       options.headers['x-axis-token'] = TFS().portalToken ?? '';
       options.headers['x-axis-app-id'] = TFS().appId ?? '';
       options.headers['x-axis-client-id'] = TFS().clientId ?? '';
@@ -49,9 +53,11 @@ class AuthInterceptor extends Interceptor {
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) async {
-    log(err.response?.toString() ?? "null body",
-        name:
-            "Auth Interceptor from Error ${err.requestOptions.baseUrl}${err.requestOptions.path}");
+    log(
+      err.response?.toString() ?? "null body",
+      name:
+          "Auth Interceptor from Error ${err.requestOptions.baseUrl}${err.requestOptions.path}",
+    );
     if (err.response?.statusCode == 401 ||
         err.response?.statusCode == 403 ||
         err.response?.statusCode == 400) {
@@ -69,14 +75,18 @@ class AuthInterceptor extends Interceptor {
   }
 
   Future<void> _retryWithNewToken(
-      RequestOptions requestOptions, ErrorInterceptorHandler handler) async {
+    RequestOptions requestOptions,
+    ErrorInterceptorHandler handler,
+  ) async {
     if (TFS().portalToken != null) {
       requestOptions.headers['Authorization'] = TFS().authorization ?? '';
       requestOptions.headers['x-api-client-id'] = TFS().appId ?? '';
       requestOptions.headers['x-subAccountID'] = TFS().clientId ?? '';
       requestOptions.headers['x-aslToken'] = TFS().portalToken ?? '';
-      requestOptions.headers['x-api-encryption-key'] =
-          encryptRsa(TFS().secretKey ?? "", TFS().publicKey ?? "");
+      requestOptions.headers['x-api-encryption-key'] = encryptRsa(
+        TFS().secretKey ?? "",
+        TFS().publicKey ?? "",
+      );
       requestOptions.headers['x-axis-token'] = TFS().portalToken ?? '';
       requestOptions.headers['x-axis-app-id'] = TFS().appId ?? '';
       requestOptions.headers['x-axis-client-id'] = TFS().clientId ?? '';
@@ -91,7 +101,9 @@ class AuthInterceptor extends Interceptor {
         //         "Auth Interceptor from Retry ${requestOptions.baseUrl}${requestOptions.path}");
         final response = await dio.fetch(requestOptions);
         String data = await decryptData(
-            TFS().secretKey!, response.data['data']['payload']);
+          TFS().secretKey!,
+          response.data['data']['payload'],
+        );
         var dataJson = jsonDecode(data);
         response.data = dataJson;
         handler.resolve(response);
@@ -102,16 +114,21 @@ class AuthInterceptor extends Interceptor {
       }
     } else {
       //No new token available, pass the original error
-      handler.next(DioException(
+      handler.next(
+        DioException(
           requestOptions: requestOptions,
-          error: 'Token expired and no new token provided'));
+          error: 'Token expired and no new token provided',
+        ),
+      );
     }
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) async {
-    String data =
-        await decryptData(TFS().secretKey!, response.data['data']['payload']);
+    String data = await decryptData(
+      TFS().secretKey!,
+      response.data['data']['payload'],
+    );
     var dataJson = jsonDecode(data);
     response.data = dataJson;
     super.onResponse(response, handler);
